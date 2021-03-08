9c62ba640fca04fd33bf41b15f20b97c40626bd2
CliffyA <>
2021-03-09 04:05:46 +1100

    adding test

---
+++ Test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+build="debugx64"
+
+if [ $(uname -m) = "aarch64" ];
+then
+	build="debuga64"
+fi
+
+cd Source
+#php "CliffyC.php" "linux_gmake"
+cd ProjectGen
+cd linux_gmake
+#make config=${build} clean
+make config=${build}
+cd ..
+cd ..
+cd ..
+./Bin/CoreTest
